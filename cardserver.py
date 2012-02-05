b4fd6e81e74756438c66a08e2d3828aaf5e63941
Bartosz Woronicz <bartosz.woronicz@gmail.com>
2012-02-05 21:54:50 +0100

    cardserver minor fixes

--- cardserver.py
+++ cardserver.py
@@ -221,29 +221,23 @@ class CardServer(threading.Thread):
         rlistnames = []
         map(lambda x: rlistnames.append(x.name), rlist)
         print "Found readers:",rlistnames
-        if rlist:
-            for r in rlist: 
-                if   'ACS ACR122U' in r.name:
+        for r in rlist: 
+            if   'ACS ACR122U' in r.name:
+                self.readertype    = READER_PCSC
+                self.readersubtype = READER_ACS
+                self.reader = r
+            elif 'OMNIKEY'     in r.name:
+                if '00 01' in r.name: # the contactless interface must be choosen
                     self.readertype    = READER_PCSC
-                    self.readersubtype = READER_ACS
+                    self.readersubtype = READER_OMNIKEY
+                    print >>sys.stderr,rlist.index(r)
                     self.reader = r
-                elif 'OMNIKEY'     in r.name:
-                    if '00 01' in r.name: # the contactless interface must be choosen
-                        self.readertype    = READER_PCSC
-                        self.readersubtype = READER_OMNIKEY
-                        print >>sys.stderr,rlist.index(r)
-                        self.reader = r
-                else:
-                    print 'No supported readers found'
-                    sys.exit(1)
-        else:
-            print 'No supported readers found'
-            sys.exit(1)
-
+            else:
+                print 'No supported readers found'
+                sys.exit(1)
 
         # Now connect to reader and attach observer
         self.connection =  self.reader.createConnection()
-        print self.connection
         # If interactive turn off showing the bytes sent
         observer = CustomCardConnectionObserver() if self.args['interactive'] else CustomCardConnectionObserver(True)
         self.connection.addObserver(observer)
